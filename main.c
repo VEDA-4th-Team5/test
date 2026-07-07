d8b1014aec6c5701a0c5cd54f7b665d0e297e18c
Hun0305 <black7599@gmail.com>
2026-07-07 10:54:11 +0900

    feat: 나는 이것이 마지막 커밋이길 희망한다 (EVDA-17)

--- main.c
+++ main.c
@@ -3,5 +3,6 @@
 int main() {
     printf("Hello, World!\n");
     printf("I'm testing my Jira integration with C17 code.\n");
+    printf("And I hope this is the last commit...\n");
     return 0;
 }
